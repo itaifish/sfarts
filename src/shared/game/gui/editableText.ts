8b3c61ead9a2c20d6e001e7f46a2e49b27b9e462
Itai Fish <chessandpotatoes@gmail.com>
2020-09-30 13:27:39 -0400

    Added placeholders to editableText

--- src/shared/game/gui/editableText.ts
+++ src/shared/game/gui/editableText.ts
@@ -6,19 +6,27 @@ import TextEdit from "phaser3-rex-plugins/plugins/textedit-plugin";
 export default class EditableText extends BBCodeText {
     edit: any;
     inputType: string;
-    innerText: string;
+    placeholder: string;
+    isPlaceHolder: boolean;
 
-    constructor(scene: any, x: number, y: number, text: string, inputType?: any) {
-        super(scene, x, y, text, EditableText.getDefaultStyle());
+    constructor(scene: any, x: number, y: number, placeholder: string, inputType?: any) {
+        super(scene, x, y, placeholder, EditableText.getDefaultStyle(true));
         this.setInteractive();
         this.inputType = inputType;
         const meInScene = scene.add.existing(this);
+        this.placeholder = placeholder;
+        this.isPlaceHolder = true;
+        this.getDefaultConfig = this.getDefaultConfig.bind(this);
         this.on("pointerdown", () => {
-            if (this.innerText == "") {
-                this.text = "\n";
+            if (this.isPlaceHolder) {
+                this.text = "";
+                this.isPlaceHolder = false;
+                this.setColor(EditableText.getDefaultStyle(false).color);
             }
-            debugger;
-            new TextEdit(scene.plugins).edit(meInScene, this.getDefaultConfig(), (textObject: EditableText) => {});
+            new TextEdit(scene.plugins).edit(meInScene, this.getDefaultConfig(), (textObject: EditableText) => {
+                // Here, this does not exist in the lambda function, so we use textObject
+                textObject.handleTextSubmit(textObject);
+            });
         });
     }
 
@@ -27,25 +35,36 @@ export default class EditableText extends BBCodeText {
             type: this.inputType || "text",
             text: this.text,
             onTextChanged: (textObject: EditableText, text: string) => {
-                this.innerText = text;
-                debugger;
-                if (text == "") {
-                    textObject.text = "\n";
-                } else if (this.inputType == "password") {
-                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                    textObject.text = [...text].reduce((acc, _currentVal) => acc + "*", "");
-                } else {
-                    textObject.text = text;
-                }
+                // Note that textObject == this
+                this.handleTextSubmit(textObject, text);
             },
         };
     }
 
-    static getDefaultStyle() {
+    handleTextSubmit(textObject: EditableText, text?: string) {
+        // Note that textObject == this
+        const existingText: string = text != null ? text : textObject.isPlaceHolder ? "" : textObject.text;
+        if (existingText == "") {
+            textObject.isPlaceHolder = true;
+            textObject.text = textObject.placeholder;
+        } else if (textObject.inputType == "password") {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            textObject.text = [...existingText].reduce((acc, _currentVal) => acc + "•", "");
+            textObject.isPlaceHolder = false;
+        } else {
+            textObject.text = existingText;
+            textObject.isPlaceHolder = false;
+        }
+        textObject.setColor(EditableText.getDefaultStyle(textObject.isPlaceHolder).color);
+    }
+
+    static getDefaultStyle(isPlaceHolder: boolean) {
         return {
-            color: "white",
+            color: isPlaceHolder ? "grey" : "white",
             fontSize: "30px",
             fixedWidth: 270,
+            valign: "center",
+            fixedHeight: 100,
         };
     }
 }
--- src/shared/game/gui/inputButton.ts
+++ src/shared/game/gui/inputButton.ts
@@ -5,7 +5,7 @@ export default class InputButton {
     constructor(scene: Phaser.Scene, x: number, y: number, text?: string, inputType?: string) {
         const image = scene.add.image(x, y, ImageKeys.BUTTON);
         image.scale = 1.5; // rescale
-        const editableText: any = new EditableText(scene, x - 105, y - 210, text, inputType);
+        const editableText: any = new EditableText(scene, x - 105, y - 240, text, inputType);
         scene.add.existing(editableText);
     }
 }
