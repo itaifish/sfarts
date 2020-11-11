bd1298e5398c2e2a6edc40c4be817194e199458d
Itai Fish <chessandpotatoes@gmail.com>
2020-11-11 14:22:02 -0500

    Added login request/response for frontend

--- src/frontend/FrontendAppComponent.tsx
+++ src/frontend/FrontendAppComponent.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 import Client from "../client/client";
-import jquery from "jquery";
 import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+import LoginForm from "./login/LoginForm";
+import MessageEnum from "../shared/communication/messageEnum";
+import { LoginMessageResponseType } from "../shared/communication/messageInterfaces/loginMessage";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface FrontendAppComponentProps {}
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface FrontendAppComponentState {
     client: Client;
+    username: string;
+    errorMessage: string;
 }
 
 class FrontendAppComponent extends React.Component<FrontendAppComponentProps, FrontendAppComponentState> {
@@ -16,15 +19,37 @@ class FrontendAppComponent extends React.Component<FrontendAppComponentProps, Fr
         super(props);
         this.state = {
             client: new Client(),
+            username: null,
+            errorMessage: null,
         };
         this.state.client.listen();
+
+        this.handleLoginButton = this.handleLoginButton.bind(this);
+    }
+
+    handleLoginButton(username: string, password: string) {
+        const client = this.state.client;
+        client.addOnServerMessageCallback(MessageEnum.LOGIN, () => {
+            if (client.loginStatus == LoginMessageResponseType.SUCCESS) {
+                this.setState({ username: username, errorMessage: null });
+                console.log("login successful");
+            } else {
+                this.setState({ errorMessage: "Username or password is incorrect" });
+            }
+        });
+        client.sendLoginAttempt(username, password);
     }
 
     render() {
+        const loginJSX = (
+            <div className="col text-center">
+                <LoginForm sendLoginRequestFunc={this.handleLoginButton} />
+                {this.state.errorMessage ? <div style={{ color: "red" }}>{this.state.errorMessage}</div> : <></>}
+            </div>
+        );
         return (
-            <div>
-                <div></div>
-                {"Hello"}
+            <div className="container-fluid">
+                <div className="row">{loginJSX}</div>
             </div>
         );
     }
---
+++ src/frontend/login/LoginForm.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+
+export interface LoginFormProps {
+    sendLoginRequestFunc: (username: string, password: string) => void;
+}
+
+export interface LoginFormState {
+    username: string;
+    password: string;
+}
+
+class LoginForm extends React.Component<LoginFormProps, LoginFormState> {
+    constructor(props: LoginFormProps) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+        };
+        this.handleLoginButton = this.handleLoginButton.bind(this);
+    }
+
+    handleLoginButton(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        event.preventDefault();
+        this.props.sendLoginRequestFunc(this.state.username, this.state.password);
+    }
+
+    render() {
+        return (
+            <form>
+                <div className="form-group">
+                    <label htmlFor="usernameInput">Username</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="usernameInput"
+                        aria-describedby="usernameHelp"
+                        placeholder="Enter username"
+                        value={this.state.username}
+                        onChange={(event) => {
+                            this.setState({ username: event.target.value });
+                        }}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="passwordInput">Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="passwordInput"
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChange={(event) => {
+                            this.setState({ password: event.target.value });
+                        }}
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary" onClick={this.handleLoginButton}>
+                    Login
+                </button>
+            </form>
+        );
+    }
+}
+
+export default LoginForm;
