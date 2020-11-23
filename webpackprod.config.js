686a66f460248b7c8b797ce205aca26eb3917199
Itai Fish <chessandpotatoes@gmail.com>
2020-11-23 00:01:25 -0500

    more webpack

---
+++ webpackprod.config.js
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/frontend/index.tsx",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ["babel-loader"],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: "file-loader",
+                query: {
+                    name: "[name].[ext]",
+                    outputPath: "./assets",
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist/bundle/"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/frontend/index.html",
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+                html5: true,
+                minifyCSS: true,
+                minifyJS: true,
+                minifyURLs: true,
+                removeComments: true,
+                removeEmptyAttributes: true,
+            },
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    optimization: {
+        splitChunks: {
+            name: "vendor",
+            chunks: "all",
+        },
+    },
+};
