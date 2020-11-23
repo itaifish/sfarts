7f436625a70862bd2bd759dc34782abf0d973b4b
Itai Rivkin-Fish <chessandpotatoes@gmail.com>
2020-11-23 18:52:49 +0000

    small update

--- runServer.sh
+++ runServer.sh
@@ -2,5 +2,5 @@
 val=$(fuser 9911/tcp)
 pid=${val/9911/tcp:/}
 echo "${pid}" | xargs kill
-echo "{\"PROD\": true}" >> process.json
+echo "{\"PROD\": true}" > process.json
 node serverDist/src/server/server.js &
