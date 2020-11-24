180d40b7a8ec648e251f3d551f5d78596308dfe4
Itai Fish <chessandpotatoes@gmail.com>
2020-11-24 17:31:53 -0500

    commit for master pr

--- serverDist/src/shared/game/move/specialAction.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SpecialActionName = void 0;
-var SpecialActionName;
-(function (SpecialActionName) {
-    SpecialActionName[SpecialActionName["ATTACK"] = 0] = "ATTACK";
-})(SpecialActionName = exports.SpecialActionName || (exports.SpecialActionName = {}));
-//# sourceMappingURL=specialAction.js.map
--- serverDist/src/shared/utility/convertToString.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const locationToString = (location) => {
-    return `${location.x}, ${location.y}`;
-};
-exports.default = locationToString;
-//# sourceMappingURL=convertToString.js.map
